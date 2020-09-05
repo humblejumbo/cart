ab2b380247234c82f86c382dae0f414082d19a6f
humblejumbo <goelshubham46@gmail.com>
2020-09-05 21:28:35 +0530

    added cart component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import CartItem from './CartItem';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Cart</h1>
+        <CartItem />
       </div>
     );
   }
